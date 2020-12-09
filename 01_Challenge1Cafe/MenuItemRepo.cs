0f46cf2ee3704272a0a2e7dc5067404b12df8298
unknown <lindseyjrobertson@gmail.com>
2020-12-08 20:11:37 -0500

    initial commit

---
+++ 01_Challenge1Cafe/MenuItemRepo.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Challenge1Cafe
+{
+    public class MenuItemRepo
+    {
+        private readonly List<MenuItem> _menuItemDirectory = new List<MenuItem>();
+
+        //Create
+        public void AddMenuItemToList(MenuItem menuItem)
+        {
+            _menuItemDirectory.Add(menuItem);
+        }
+
+        //Read
+        public List<MenuItem> GetMenuList()
+        {
+            return _menuItemDirectory;
+        }
+
+        //Update
+        public bool UpdateMenuItemInfo(int existingMenuNumber, MenuItem newMenuItem)
+        {
+            //find the menu item to update
+            MenuItem oldInfo = GetMenuItemByNumber(existingMenuNumber);
+
+            //update menu item
+            if (oldInfo != null)
+            {
+                oldInfo.MealNumber = newMenuItem.MealNumber;
+                oldInfo.MealName = newMenuItem.MealName;
+                oldInfo.Description = newMenuItem.Description;
+                oldInfo.Ingredients = newMenuItem.Ingredients;
+                oldInfo.Price = newMenuItem.Price;
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        //Delete
+        public bool RemoveMenuItemFromList(int mealNumber)
+        {
+            MenuItem menuItem = GetMenuItemByNumber(mealNumber);
+            if (menuItem == null)
+            {
+                return false;
+            }
+
+            int initialMenuItemCount = _menuItemDirectory.Count;
+            _menuItemDirectory.Remove(menuItem);
+
+            if (initialMenuItemCount > _menuItemDirectory.Count)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        //Get By Number
+        public MenuItem GetMenuItemByNumber(int mealNumber)
+        {
+            foreach (MenuItem menuItem in _menuItemDirectory)
+            {
+                if (menuItem.MealNumber == mealNumber)
+                {
+                    return menuItem;
+                }
+            }
+            return null;
+        }
+    }
+}
+
---
+++ 01_Challenge1CafeConsoleApp/ProgramUI.cs
@@ -0,0 +1,78 @@
+﻿using _01_Challenge1Cafe;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Challenge1CafeConsoleApp
+{
+    class ProgramUI
+    {
+        private MenuItemRepo _menuItemRepo = new MenuItemRepo();
+
+
+        public void Run()
+        {
+            //SeedMenuList();
+            Menu();
+        }
+
+        private void Menu()
+        {
+            bool keepRunning = true;
+            while (keepRunning)
+            {
+                Console.WriteLine("Welcome to the Komodo Cafe Management Application. \nPlease choose from the following options:\n" +
+                    "\n1. View All Menu Items\n" +
+                    "2. Add New Menu Item\n" +
+                    "3. Delete Menu Item\n" +
+                    "4. Exit");
+
+                string input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        //View Menu Items
+
+                        break;
+                    case "2":
+                        //Add Menu Items
+                        break;
+                    case "3":
+                        //Delete Menu Items
+                        break;
+                    case "4":
+                        Console.WriteLine("Thank you for using Komodo Cafe Management Application. Goodbye!");
+                        keepRunning = false;
+                        break;
+                    default:
+                        Console.WriteLine("Your selection was invalid. Please enter a valid number.");
+                        break;
+                }
+                Console.WriteLine("Please press any key to continue...");
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+
+        public void ViewMenuItems()
+        {
+            Console.Clear();
+            GetMenuList();
+
+        }
+
+        public void AddMenuItems()
+        {
+
+        }
+
+        public void DeleteMenuItems()
+        {
+
+        }
+    }
+}
+
